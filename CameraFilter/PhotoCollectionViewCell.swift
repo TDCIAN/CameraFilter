c4d5394ff36905b5fea0617d677b4cbadf0e5f57
TDCIAN <tdcian71@gmail.com>
2022-04-30 15:15:33 +0900

    modified

---
+++ CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by JeongminKim on 2022/04/29.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    static let identifer = "PhotoCollectionViewCell"
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
